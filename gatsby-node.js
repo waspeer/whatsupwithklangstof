da96f106e823f114fd54308ddfe8c909a48cfc4d
Wannes Salomé <mail@wannessalome.nl>
2020-02-07 22:42:02 +0100

    Customize graphql type for yaml posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,16 @@
  */
 
 // You can delete this file if you're not using it
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions;
+
+  const typeDefs = `
+    type PostsYaml implements Node {
+      title: String
+      date: Date
+      url: String
+    }
+  `;
+
+  createTypes(typeDefs);
+};
