f6f4ffc7b1265c27e04d2e067039eac0e2bf6458
Wannes Salomé <mail@wannessalome.nl>
2020-02-08 18:14:54 +0100

    Add eslint plugin for cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-  extends: ["./node_modules/poetic/config/eslint/eslint-config.js"],
+  extends: ['./node_modules/poetic/config/eslint/eslint-config.js', 'plugin:cypress/recommended'],
   // Add custom rules here
   rules: {
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "comma-dangle": ["warn", "always-multiline"],
-    "semi": ["warn", "always"],
-    "object-curly-spacing": ["warn", "always"],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'comma-dangle': ['warn', 'always-multiline'],
+    semi: ['warn', 'always'],
+    'object-curly-spacing': ['warn', 'always'],
   },
   settings: {
-    "import/resolver": {
-      typescript: {}
-    }
-  }
+    'import/resolver': {
+      typescript: {},
+    },
+  },
 };
