ab9cee6699953ffed47d0fcea42d7b31e8f2b4e2
Wannes Salomé <mail@wannessalome.nl>
2020-02-10 21:37:08 +0100

    Add jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  globals: { __PATH_PREFIX__: `` },
+  moduleNameMapper: {
+    '^#components/(.*)$': '<rootDir>/src/components/$1',
+  },
+  setupFiles: [`<rootDir>/test/jest/loadershim.js`],
+  setupFilesAfterEnv: [`<rootDir>/test/jest/setup.js`],
+  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
+  testRegex: '(/test/jest/|/src/).*\\.(test|spec)\\.(ts|tsx|js|jsx)$',
+  transform: {
+    '^.+\\.[jt]sx?$': '<rootDir>/test/jest/jest-preprocess.js',
+  },
+  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+};
