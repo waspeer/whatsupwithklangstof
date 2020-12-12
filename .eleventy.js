f1fc0dbcc6a3e08cd8b4960b20ab87669c5d1996
Wannes Salomé <mail@wannessalome.nl>
2020-12-12 16:57:04 +0100

    Improve + add socials

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,13 @@
+const { FontAwesomeIcon } = require('@campj/eleventy-fa-icons');
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ 'src/public': '/' });
+  eleventyConfig.addNunjucksShortcode('FontAwesomeIcon', FontAwesomeIcon);
 
   return {
     dir: {
       input: 'src',
+      output: 'dist',
     },
   };
 };
