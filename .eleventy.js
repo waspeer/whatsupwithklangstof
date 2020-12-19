f126d9726e6a3a8f0068a7491b10d88ac1104383
Wannes Salomé <mail@wannessalome.nl>
2020-12-19 22:37:20 +0100

    Add Netlify CMS

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const { FontAwesomeIcon } = require('@campj/eleventy-fa-icons');
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('admin');
   eleventyConfig.addPassthroughCopy({ 'src/public': '/' });
   eleventyConfig.addNunjucksShortcode('FontAwesomeIcon', FontAwesomeIcon);
 
